77ad5a95ca89900d44642b0e124d9241a2641f3a
Nitin Srivastava <nitin@curvetomorrow.com>
2021-01-27 23:16:51 +0530

    Geocoder for Point and View models

--- app/models/view.rb
+++ app/models/view.rb
@@ -2,4 +2,7 @@ class View < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :tour, optional: true
   belongs_to :point, optional: true
+
+  geocoded_by :full_address
+  after_validation :geocode, if: ->(obj) { obj.full_address.present? and obj.full_address_changed? }
 end
