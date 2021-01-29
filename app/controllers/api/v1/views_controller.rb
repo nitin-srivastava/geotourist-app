6db083a0cf4345c6aea3ce833f8948d9379648a8
Nitin Srivastava <nitin@curvetomorrow.com>
2021-01-29 10:38:00 +0530

    Views endpoints have been modified

--- app/controllers/api/v1/views_controller.rb
+++ app/controllers/api/v1/views_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::ViewsController < Api::ApplicationController
   before_action :set_view, only: :update
-  before_action :set_tour, only: %i[create update]
 
   def create
-    @view = @tour.views.new(view_params)
+    @view = View.new(view_params)
     if @view.save
       @view.update_analytics
       render nothing: true, status: :ok
@@ -25,11 +24,6 @@ class Api::V1::ViewsController < Api::ApplicationController
   def set_view
     @view = View.find_by(id: params[:id])
     render nothing: true, status: :not_found unless @view.present?
-  end
-
-  def set_tour
-    @tour = Tour.find_by(id: params[:tour_id])
-    render nothing: true, status: :not_found unless @tour.present?
   end
 
   def view_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :tours, only: :update do
-        resources :views, only: %i[create update]
-      end
+      resources :tours, only: :update
+      resources :views, only: %i[create update]
       resources :users, only: :update
       resources :points, only: :update
       resources :visits, only: :create
